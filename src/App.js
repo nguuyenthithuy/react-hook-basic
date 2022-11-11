223821c5c8e7092274cec025e6f89ebaa5a5e143
Dat Thuy <syn8749@gmail.com>
2022-11-11 16:02:18 +0700

    add event

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import './App.css';
 import Nav from './views/Nav.js';
 const App = () => {
   let name = 'Thủy';
-  let obj = { name: 'Thủy', age: 19 }
-  let link = ' https://www.facebook.com/profile.php?id=100016430322265 '
+
+  const handleOnClickme = (event) => {
+    console.log('>>>click me', event.target.value)
+  }
+
+
   return (
     <div className="App">
       <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello Nguyễn Thị Thủy with Hook </h1>
+        <h1>Hello Nguyễn Thị {name} with Hook </h1>
 
-        <p style={{ color: 'red', fontSize: '20px', marginTop: '15px' }}>{JSON.stringify(obj)}</p>
-
-        <a href={link} target="_blank"> Nhấn vào đây để xem facebook</a>
+        <input type="text" value="Thủy" onClick={(event) => handleOnClickme(event)} />
+        <button type='button' onClick={(event) => handleOnClickme(event)}>Click</button>
       </header>
     </div>
   );
