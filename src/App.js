bdd74ab0867012f503215d9a08513ef428be9fac
Dat Thuy <syn8749@gmail.com>
2022-11-11 21:09:34 +0700

    useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './views/Nav.js';
+import { useState } from 'react';
+
+
 const App = () => {
-  let name = 'Thủy';
+  let [name, setName] = useState('Thủy'); // [a1,b1,c1.....]
 
   const handleOnClickme = (event) => {
-    console.log('>>>click me', event.target.value)
+    setName(Adress)
+  }
+
+  const [Adress, setAdress] = useState('');
+
+  const handleOnchaneInput = (event) => {
+    setAdress(event.target.value)
   }
 
 
@@ -16,7 +25,7 @@ const App = () => {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello Nguyễn Thị {name} with Hook </h1>
 
-        <input type="text" value="Thủy" onClick={(event) => handleOnClickme(event)} />
+        <input type="text" value={Adress} onChange={(event) => handleOnchaneInput(event)} />
         <button type='button' onClick={(event) => handleOnClickme(event)}>Click</button>
       </header>
     </div>
